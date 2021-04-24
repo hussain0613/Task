bcda0c24e254be55d1e7fa6efd144b2658c1ec4d
Muhammad Hossain <hussain0613@gmail.com>
2021-04-24 07:04:46 +0600

    basic chat through web socket done :D

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, WebSocket, Request
+from fastapi import FastAPI, WebSocket, Request, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -14,9 +14,21 @@ async def get(request:Request):
     return templates.TemplateResponse("ok.html", context = {'request': request})
 
 
+websockets = []
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(uname, websocket: WebSocket):
     await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+    await websocket.send_text(f"[*] welcome {uname}")
+    websockets.append(websocket)
+    for ws in websockets:
+        if(ws != websocket): await ws.send_text(f"[*] {uname} has joined")
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"You: {data}")
+            for ws in websockets:
+                if(ws != websocket): await ws.send_text(f"{uname}: {data}")
+    except WebSocketDisconnect as err:
+        websockets.remove(websocket)
+        for ws in websockets:
+            await ws.send_text(f"[*] {uname} disconnect")
--- app/statics/ok.js
+++ app/statics/ok.js
@@ -1,14 +1,50 @@
-var ws = new WebSocket("ws://localhost:8000/ws");
-ws.onmessage = function(event) {
-    var messages = document.getElementById('messages')
-    var message = document.createElement('li')
-    var content = document.createTextNode(event.data)
-    message.appendChild(content)
-    messages.appendChild(message)
-};
-function sendMessage(event) {
-    var input = document.getElementById("messageText")
-    ws.send(input.value)
-    input.value = ''
-    event.preventDefault()
+class Session{
+    constructor(ws){
+        this.ws = ws;
+    }
+
+    sendMessage(event) {
+        var input = document.getElementById("messageText")
+        ws.send(input.value)
+        input.value = ''
+        event.preventDefault()
+    }
+}
+
+
+
+function login() {
+    ws = new WebSocket(location.origin.replace(location.protocol, "ws:")+"/ws?uname="+ document.getElementById('uname').value);
+    document.getElementById('authbox').style.display='none'
+    document.getElementById('logout_btn').style.display='block'
+    document.getElementById('chatbox').style.display='block'
+
+
+    ws.onmessage = function(event) {
+        var messages = document.getElementById('messages')
+        var message = document.createElement('li')
+        var content = document.createTextNode(event.data)
+        message.appendChild(content)
+        messages.appendChild(message)
+    };
+
+    ws.onclose = function name(event) {
+        var messages = document.getElementById('messages')
+        var message = document.createElement('li')
+        var content = document.createTextNode("[*] server disconnected")
+        message.appendChild(content)
+        messages.appendChild(message)
+    }
+    session = new Session(ws);
+    //event.preventDefault()
+    return session;
+}
+
+function logout() {
+    ws.close()
+    document.getElementById('authbox').style.display='block'
+    document.getElementById('logout_btn').style.display='none'
+    document.getElementById('chatbox').style.display='none'
+    //event.preventDefault()
+    return false;
 }
